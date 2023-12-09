26b3d2802c8cdbb30df74ac5421f61881fa524ea
mothibimalebana <mothibi.mm12@gmail.com>
2023-12-09 08:29:00 +0200

    Tried adding multer, messed up the project

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,14 +1,27 @@
 // Import Modules
 const express = require("express");
 
+
 //Create
 const routes = express.Router();
 
 // Routes
-routes.post("/", (req, res) => {
-  res.json({
-    Message: "Create new event",
-  });
+routes.post("/", upload.single("image"), async (req, res) => {
+  try {
+    const { title, link } = req.body;
+    const image = {
+      data: req.file.buffer,
+      contentType: req.file.mimetype,
+    };
+
+    const event = new Event({ title, link, image });
+    await event.save();
+
+    res.status(201).json({ message: "Event created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 routes.get("/", (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const eventRoutes = require("./routes/eventRoutes");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const Grid = require("gridfs-stream");
+const path = require("path");
 
 // Run express app
 const app = express();
@@ -10,7 +14,29 @@ const app = express();
 app.use(express.json());
 app.use("/events", eventRoutes);
 
-//Set up Port
-app.listen(process.env.PORT, () => {
-  console.log("Listening on Port:" + process.env.PORT);
+mongoose
+  .connect(process.env.MongoDB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //Set up Port
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB and Listening on Port:" + process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const conn = mongoose.connection;
+
+let gfs;
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
 });
+
+// Multer configuration for file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
