2d142203a723abd0fe4e9145e16eaeb42ec84a2e
mothibimalebana <mothibi.mm12@gmail.com>
2023-12-10 10:44:25 +0200

    set up port:4000

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 //Importing modules
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 
 //Initiate server app
-const app = express()
+const app = express();
 
 //Listening
-app.listen()
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port: " + process.env.PORT);
+});
